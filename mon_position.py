878314f8d22bf761ae3ec7ddefcc2cc2df5f97c0
tpmmthomas <71880685+tpmmthomas@users.noreply.github.com>
2021-07-28 12:32:12 +0800

    Update mon_position.py

--- mon_position.py
+++ mon_position.py
@@ -332,7 +332,7 @@ class FetchLatestPosition(threading.Thread):
                     self.driver.refresh()
                 except:
                     self.error += 1
-                    time.sleep(30)
+                    time.sleep(45)
                     continue
             time.sleep(5)
             soup = BeautifulSoup(self.driver.page_source,features="html.parser")
@@ -359,7 +359,7 @@ class FetchLatestPosition(threading.Thread):
                 if self.num_no_data != 1:
                     self.prev_df = "x"
                     self.first_run = False
-                    time.sleep(30)
+                    time.sleep(45)
                 time.sleep(5)
                 self.runtimes += 1
                 if self.runtimes >=15:
@@ -413,7 +413,7 @@ class FetchLatestPosition(threading.Thread):
                 self.runtimes = 0
                 self.driver.quit()
                 self.driver = None
-            time.sleep(30)
+            time.sleep(45)
         if self.driver is not None:
             self.driver.quit()
         updater.bot.sendMessage(chat_id=self.chat_id,text=f"Successfully quit following trader {self.name}.")
