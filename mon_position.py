b5dfbef21c2a563fbb717c12323a51b9217f1b33
tpmmthomas <71880685+tpmmthomas@users.noreply.github.com>
2021-08-05 14:54:30 +0800

    Update mon_position.py

--- mon_position.py
+++ mon_position.py
@@ -89,7 +89,7 @@ logger = logging.getLogger(__name__)
 ) = range(50)
 CurrentUsers = {}
 updater = Updater(cnt.bot_token)
-master_lock = threading.Semaphore(2)
+master_lock = threading.Semaphore(3)
 mutex = threading.Lock()
 options = webdriver.ChromeOptions()
 options.binary_location = cfg.chrome_location
@@ -477,8 +477,8 @@ class FetchLatestPosition(threading.Thread):
                     master_lock.release()
                     time.sleep(75)
                     continue
+            time.sleep(3)
             master_lock.release()
-            time.sleep(4)
             soup = BeautifulSoup(self.driver.page_source, features="html.parser")
             x = soup.get_text()
             ### THIS PART IS ACCORDING TO THE CURRENT WEBPAGE DESIGN WHICH MIGHT BE CHANGED
