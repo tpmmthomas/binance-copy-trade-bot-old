882aeecb3ff6dcfb496552c55533cee81e1c06f7
tpmmthomas <71880685+tpmmthomas@users.noreply.github.com>
2021-09-06 10:36:21 +0800

    Update mon_position.py

--- mon_position.py
+++ mon_position.py
@@ -3217,7 +3217,7 @@ def reload_updater():
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
     # start_polling() is non-blocking and will stop the bot gracefully.
-    updater.idle()
+    # updater.idle()
 
 
 class AAXClient:
